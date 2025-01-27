f03edec9c3c4fa2fce978496a8f2609319450668
Alice W. <wenyaxuan0925@outlook.com>
2025-01-27 12:44:18 -0500

    type

--- src/RawPOSIX/src/rawposix/syscalls/sys_calls.rs
+++ src/RawPOSIX/src/rawposix/syscalls/sys_calls.rs
@@ -1,29 +1,33 @@
 use crate::cage::*;
 use std::sync::atomic::Ordering;
+use std::sync::atomic::Ordering::*;
 use crate::fdtables;
 
 pub fn fork_syscall(cageid: u64, child_cageid: u64, _arg2: u64, _arg3: u64, _arg4: u64, _arg5: u64, _arg6: u64) -> i32 {
     // Modify the fdtable manually 
-    fdtables::copy_fdtable_for_cage(self.cageid, child_cageid).unwrap();
+    fdtables::copy_fdtable_for_cage(cageid, child_cageid).unwrap();
 
-    let parent_vmmap = self.vmmap.read();
+    // Get the self cage
+    let selfcage = get_cage(cageid).unwrap();
+
+    let parent_vmmap = selfcage.vmmap.read();
     let new_vmmap = parent_vmmap.clone();
 
     let cageobj = Cage {
         cageid: child_cageid,
-        cwd: RwLock::new(self.cwd.read().clone()),
-        parent: self.cageid,
+        cwd: RwLock::new(selfcage.cwd.read().clone()),
+        parent: cageid,
         gid: AtomicI32::new(
-            self.gid.load(Ordering::Relaxed),
+            selfcage.gid.load(Ordering::Relaxed),
         ),
         uid: AtomicI32::new(
-            self.uid.load(Ordering::Relaxed),
+            selfcage.uid.load(Ordering::Relaxed),
         ),
         egid: AtomicI32::new(
-            self.egid.load(Ordering::Relaxed),
+            selfcage.egid.load(Ordering::Relaxed),
         ),
         euid: AtomicI32::new(
-            self.euid.load(Ordering::Relaxed),
+            selfcage.euid.load(Ordering::Relaxed),
         ),
         main_threadid: AtomicU64::new(0),
         zombies: RwLock::new(vec![]),
