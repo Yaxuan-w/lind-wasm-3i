39c4ef2af6feb5a36e00c6f2c579c27ec7d94818
Alice W. <wenyaxuan0925@outlook.com>
2025-02-20 18:39:42 -0500

    Attempt: call grate from 3i

--- src/wasmtime/src/commands/run.rs
+++ src/wasmtime/src/commands/run.rs
@@ -580,7 +580,7 @@ impl RunCommand {
                 //     eprintln!("c_test_func not found in Wasm instance!");
                 // }
                 let table = instance
-                    .get_export(&mut store, "__indirect_function_table")
+                    .get_export(&mut *store, "__indirect_function_table")
                     .and_then(|e| e.into_table())
                     .expect("Table not found");
 
