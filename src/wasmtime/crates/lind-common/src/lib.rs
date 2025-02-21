a7dfb640743d3a374694fd3b2fe159d29526a8aa
Alice W. <wenyaxuan0925@outlook.com>
2025-02-21 17:21:11 -0500

    Attempt: add register_handler as syscall

--- src/wasmtime/crates/lind-common/src/lib.rs
+++ src/wasmtime/crates/lind-common/src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
 use anyhow::Result;
-use threei::threei::{make_syscall, threei_test_func};
+use threei::threei::{make_syscall, threei_test_func, register_handler};
 use wasmtime_lind_multi_process::{get_memory_base, LindHost, clone_constants::CloneArgStruct};
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
