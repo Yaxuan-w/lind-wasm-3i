1552eb85e6984a86f451d999838d23bedb432e28
Alice W. <wenyaxuan0925@outlook.com>
2025-02-21 17:37:13 -0500

    Attempt: treat registerhandler as syscall wrapper

--- pocadd.c
+++ pocadd.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/syscall.h>  
 
 int pass_fptr_to_wt() {
     int fd = open("testfile.txt", O_CREAT | O_WRONLY, 0644);
@@ -16,7 +17,7 @@ int main() {
     int targetcallnum = 1;
     void *handlefunc = NULL;
     int handlefunccage = 1;
-    int ret = registerhandler(targetcageid, targetcallnum, handlefunc, handlefunccage);
+    int ret = syscall(400, targetcageid, targetcallnum, handlefunc, handlefunccage);
     if (ret == 1) {
         printf("[GRATE] registerhandler succeed!");
     } else {
