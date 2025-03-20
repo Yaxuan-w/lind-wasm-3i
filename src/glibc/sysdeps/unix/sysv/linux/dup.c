fb18cd4606a3b7e21a0e5377b67b9a32151edeca
Mohd Sarfaraz Faiyaz <102454287+bblackheart013@users.noreply.github.com>
2025-03-20 20:54:23 +0000

    Addressed review comments: Removed redundant line in lind_syscall_num.h and improved formatting in dup.c as per feedback.

--- src/glibc/sysdeps/unix/sysv/linux/dup.c
+++ src/glibc/sysdeps/unix/sysv/linux/dup.c
@@ -1,25 +1,32 @@
 /* Linux dup syscall implementation.
    Copyright (C) 2017-2024 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
+
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
+
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
    Lesser General Public License for more details.
+
    You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library.  If not, see
-   <https://www.gnu.org/licenses/>.  */
+   License along with the GNU C Library. If not, see
+   <https://www.gnu.org/licenses/>.  
+*/
+
 #include <unistd.h>
 #include <sysdep.h>
 #include <syscall-template.h>
 #include <lind_syscall_num.h>
-/* Duplicate FD to a new file descriptor.  */
+
+/* Duplicate FD to a new file descriptor. */
 int
 __dup (int fd)
 {
    return MAKE_SYSCALL(DUP_SYSCALL, "syscall|dup", (uint64_t) fd, NOTUSED, NOTUSED, NOTUSED, NOTUSED, NOTUSED);
 }
 libc_hidden_def (dup)
+
